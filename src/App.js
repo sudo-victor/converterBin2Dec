1257abf8af150c630ec52dd1bb866280f631ff17
Victor de Souto <victor.souto@hotmail.com>
2020-03-20 23:42:33 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import {
+    GlobalStyles,
+    Container,
+    Form,
+    Label,
+    SpanError,
+    Input,
+    Output,
+    Button
+} from "./styles";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [messageError, setMessageError] = useState("");
+    const [binaryInput, setBinaryInput] = useState("");
+    const [decimalOutput, setDecimalOutput] = useState("");
+
+    const binToDec = e => {
+        e.preventDefault();
+        if (binaryInput.match(/^[0-1]+$/g) === null) {
+            setMessageError("Enter either 0 or 1");
+            return;
+        }
+
+        setMessageError("");
+
+        const transformString = binaryInput
+            .split("")
+            .map(Number)
+            .reverse();
+
+        setDecimalOutput(transformString);
+    };
+
+    return (
+        <>
+            <GlobalStyles />
+            <Container className="App">
+                <h1>Bin2Dec</h1>
+                <Form>
+                    <SpanError>{messageError}</SpanError>
+                    <Label>Binary Input</Label>
+                    <Input
+                        type="text"
+                        name=""
+                        id=""
+                        value={binaryInput}
+                        onChange={e => setBinaryInput(e.target.value)}
+                        placeholder="Type a number binary"
+                    />
+                    <Button onClick={binToDec}>to convert</Button>
+                    <Label>Decimal Output</Label>
+                    <Output
+                        type="text"
+                        name=""
+                        id=""
+                        value={decimalOutput}
+                        placeholder="Number Decimal"
+                        disabled
+                    />
+                </Form>
+            </Container>
+        </>
+    );
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,88 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    *, html, body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        font-family: 'Noto Sans', sans-serif;
+        color: #f9f9f9;
+    }
+`;
+
+export const Container = styled.div`
+    padding: 20px;
+    height: 100vh;
+
+    background: #f9f9f9;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Form = styled.form`
+    width: 50%;
+    margin: 20px auto;
+    padding: 5px 5px 30px 5px;
+
+    background: #333;
+    border-radius: 5px;
+    box-shadow: 5px 5px 7px #333;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const SpanError = styled.span`
+    margin: 5px;
+    padding: 2px;
+
+    color: tomato;
+    font-size: 13px;
+`;
+
+export const Label = styled.label`
+    margin: 5px;
+
+    font-weight: 700;
+`;
+
+export const Input = styled.input`
+    width: 50%;
+    height: 40px;
+    margin: 5px;
+    padding: 20px;
+    text-align: center;
+
+    border: none;
+    border-radius: 5px;
+    box-shadow: 2px 2px 5px #222;
+    color: #333;
+`;
+
+export const Output = styled(Input)`
+    background: #fff;
+`;
+
+export const Button = styled.button`
+    width: 200px;
+    height: 50px;
+    margin: 15px;
+
+    border: none;
+    border-radius: 50px;
+    color: #f5f5f5;
+    background: tomato;
+    font-weight: 700;
+
+    transition: all 800ms;
+
+    &:hover {
+        background: #f9f9f9;
+        color: tomato;
+    }
+`;
